cf171bdc1f125f71b064c428f8023a9947eadd9d
Unlim12 <unlim12345@gmail.com>
2025-02-24 13:05:51 +0200

    fix: Change Favicon/title

--- src/App.vue
+++ src/App.vue
@@ -4,40 +4,24 @@ import HomeView from './views/HomeView.vue'
 </script>
 
 <template>
-<!--  <header>-->
-<!--    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />-->
-<!--  </header>-->
-
   <main>
+    <h1>Frozeneon Frontend Test Task<br> Shestakov Bohdan</h1>
     <HomeView />
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+h1 {
+  max-width: 100%;
+  width: 100%;
+  white-space: pre-wrap;
+  word-break: break-word;
+  caret-color: rgb(55, 53, 47);
+  padding-top: 3px;
+  padding-left: 2px;
+  padding-right: 2px;
+  font-size: 40px;
+  font-weight: inherit;
+  text-align: center;
 }
 </style>
